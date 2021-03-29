b6aeb32bf70f19eb30664516aec917b32ed83f12
lewis mcmillan <lewistmcmillan@gmail.com>
2021-03-29 16:54:49 +0100

    First working Autoencoder model.

--- ml.py
+++ ml.py
@@ -19,14 +19,18 @@ import vca
 
 
 class RMSELoss(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1e-8):
         super(RMSELoss, self).__init__()
+        self.eps = eps
 
     def forward(self, x, y):
-        eps = 1e-6
-        criterion = nn.MSELoss(reduce="sum")
-        loss = torch.sqrt(criterion(x, y) + eps)
+        """
+        https://gist.github.com/jamesr2323/33c67ba5ac29880171b63d2c7f1acdc5
+        """
+
+        loss = torch.sqrt(F.mse_loss(x, y) + self.eps)
         return loss
+
 
 class SADLoss(nn.Module):
     def __init__(self):
@@ -35,8 +39,8 @@ class SADLoss(nn.Module):
 
     def forward(self, x, y):
         """
-        cos = nn.CosineSimilarity(dim=1, eps=1e-08)
-        output = cos(input1, input2)
+        spectral angle distance
+        see https://pytorch.org/docs/stable/generated/torch.nn.CosineSimilarity.html#torch.nn.CosineSimilarity
 
         shape: [batch, bands]
 
@@ -55,7 +59,7 @@ class SIDLoss(nn.Module):
 
     def forward(self, x, y):
         """
-
+        https://douglasrizzo.com.br/kl-div-pytorch/
         """
         a = F.kl_div(x.log_softmax(0), y.softmax(0), reduction="sum")  # input needs to be logged, target does not...
         b = F.kl_div(y.log_softmax(0), x.softmax(0), reduction="sum")
@@ -91,103 +95,122 @@ class normIt(nn.Module):
         return x
 
 
-class LitAutoEncoder(pl.LightningModule):
+class PosLinear(nn.Module):
+    def __init__(self, in_dim, out_dim, weights):
+        super(PosLinear, self).__init__()
+        self.weight = nn.Parameter(torch.transpose(weights, 1, 0))
 
-    def __init__(self, input_size, end_members):
-        super().__init__()
+    def forward(self, x):
+        return torch.matmul(x, torch.abs(self.weight))
+
+
+class DAN(pl.LightningModule):
+    """docstring for DAN"""
+    def __init__(self, input_size, end_members, weights, train=True):
+        super(DAN, self).__init__()
+        self.input_size = input_size
+        self.end_members = end_members
+        self.weights = weights
+
         self.encoder = nn.Sequential(
-            nn.Linear(input_size, 9*end_members),
-            nn.LeakyReLU(),
-            nn.Linear(9*end_members, 6*end_members),
-            nn.LeakyReLU(),
-            nn.Linear(6*end_members, 3*end_members),
-            nn.LeakyReLU(),
-            nn.Linear(3*end_members, end_members),
-            nn.LeakyReLU(),
+            nn.Linear(input_size, 32*end_members),
+            nn.ReLU(),
+            nn.Linear(32*end_members, 16*end_members),
+            nn.ReLU(),
+            nn.Linear(16*end_members, 4*end_members),
+            nn.ReLU(),
+            nn.Linear(4*end_members, end_members),
             nn.BatchNorm1d(end_members),
-            nn.LogSoftmax())
+            normIt())
+
+        # def init_weights(m):
+        #     # https://discuss.pytorch.org/t/initialize-nn-linear-with-specific-weights/29005/6
+        #     # init weights with vca guess of end members
+        #     if type(m) == nn.Linear and m.bias is None:
+        #         with torch.no_grad():
+        #             m.weight.copy_(self.weights)
 
         self.decoder = nn.Sequential(
-            nn.Linear(end_members, input_size)
-        )
+            PosLinear(end_members, input_size, weights))
+        # if train:
+        #     self.decoder.apply(init_weights)
 
-        self.RMSEloss = RMSELoss()
+        self.save_hyperparameters("input_size", "end_members")
+        # self.loss_func = RMSELoss()
+        self.loss_func = SADLoss()
+        # self.loss_func = SIDLoss()
 
     def forward(self, x):
         # in lightning, forward defines the prediction/inference actions
         embedding = self.encoder(x)
-        return embedding
+        output = self.decoder(embedding)
+
+        return embedding, output, self.decoder[0].weight
 
     def training_step(self, batch, batch_idx):
         x = batch
         x = x.float()
-        z = self.encoder(x.float())
+        z = self.encoder(x)
         x_hat = self.decoder(z)
-        # print(z)
-        # stop
-        # loss = F.mse_loss(x_hat, x)
-        loss = self.RMSEloss(x, x_hat)
+
+        # loss = F.mse_loss(x, x_hat)
+        loss = self.loss_func(x, x_hat)
+
         self.log("loss", loss)
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-2)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
 
 
 def cli_main():
-
-    # tmp = torch.Tensor([[.5, .25, .1], [0., .75, .3]])
-    # print(tmp.shape)
-    # print("yo", tmp, torch.sum(tmp, axis=1))
-    # for i in range(tmp.shape[0]):
-    #     print(tmp[i] / torch.sum(tmp, axis=1)[i])
-    # stop
-
+    torch.autograd.set_detect_anomaly(True)
     pl.seed_everything(1234)
     tb_logger = pl_loggers.TensorBoardLogger('logs/')
-    trainer = pl.Trainer(logger=tb_logger, gpus=1, auto_select_gpus=True,max_epochs=100)
-    # ------------
-    # args
-    # ------------
-    # parser = ArgumentParser()
-    # parser.add_argument('--batch_size', default=64, type=int)
-    # parser.add_argument('--hidden_dim', type=int, default=128)
-    # parser = trainer.add_argparse_args(parser)
-    # args = parser.parse_args()
+    trainer = pl.Trainer(logger=tb_logger, gpus=1, auto_select_gpus=True, max_epochs=50)
 
-    # ------------
-    # data
-    # ------------
-    # mnist_train, mnist_val = random_split(dataset, [55000, 5000])
+    # shape = (610, 340, 103) 103==bands, 9 pixel types
+    # dataset = customdata.PaviaUDataset("PaviaU.mat", transform=transforms.Compose([customdata.ToTensor()]))
+    dataset = customdata.ArtificalDataset((128, 128, 18), 20,
+                                          transform=transforms.Compose([customdata.ToTensor()]))
+    train_loader = DataLoader(dataset, batch_size=256, num_workers=32, shuffle=True)
 
-    # dataset = customdata.ArtificalDataset((24, 25, 18), 20,
-    #                                       transform=transforms.Compose([customdata.ToTensor()]))
-
-    dataset = customdata.PaviaUDataset("PaviaU.mat")
-
-    train_loader = DataLoader(dataset, batch_size=32, num_workers=32)
-    # val_loader = DataLoader(mnist_val, batch_size=64, num_workers=32)
-    test_loader = DataLoader(dataset, batch_size=64, num_workers=32)
-    val_loader = DataLoader(dataset, batch_size=64, num_workers=32)
+    # test_loader = DataLoader(dataset, batch_size=64, num_workers=32)
+    val_loader = DataLoader(dataset, batch_size=600, num_workers=32)
 
     # ------------
     # model
     # ------------
-    model = LitAutoEncoder(103, 4)
+    # data = dataset.HCI
+    data = dataset.images
+    data = np.transpose(data, (1, 0))
+    weights, _, _ = vca.vca(data, 4)
+    for i in range(4):
+        weights[:, i] /= np.max(weights[:, i])
+    model = DAN(dataset.shape[1], 4, torch.from_numpy(weights).float(), train=False)
 
     # ------------
     # training
     # ------------
-    # trainer = trainer.from_argparse_args(args)
     trainer.fit(model, train_loader, val_loader)
+    model.eval()
+    input_data = dataset[10000].unsqueeze(0).float()
+    embed, out, endmem = model.forward(input_data)
+    print(embed)
 
-    # ------------
-    # testing
-    # ------------
-    result = trainer.test(test_dataloaders=test_loader)
-    print(result)
+    plt.plot(torch.abs(out.detach().cpu()[0]))
+    plt.plot(input_data[0], label="real")
+    plt.legend()
+    plt.show()
+    for i in range(4):
+        plt.plot(weights[:, i])
+        plt.plot(endmem[i, :].detach().cpu().numpy())
+    plt.show()
+    stop
 
 
 if __name__ == '__main__':
+    plt.switch_backend('Qt5Agg')
+
     cli_main()
